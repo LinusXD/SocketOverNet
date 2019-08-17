651a181fb9775396cde390af54e1d41c43c703c4
LinusX <tghfding@163.com>
2019-08-17 16:17:26 +0800

    add methods for tcp server & client.

--- tcp/client.c
+++ tcp/client.c
@@ -1,3 +1,62 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 
+static const char servIp[] = "127.0.0.1";
+static const unsigned short port = 6666;
+#define MAX_BUFF_SIZE 4096
+
+int main(int ac, char** av)
+{
+    int sockFd;
+    int len;
+    struct sockaddr_in servAddr;
+    char buffer[MAX_BUFF_SIZE] = {0};
+    
+    // Create a socket for client.
+    sockFd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(-1 == sockFd)
+    {
+        printf("Socket create failed!\n");
+        exit(-1);
+    }
+
+    memset(&servAddr, 0, sizeof(struct sockaddr_in));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = inet_addr(servIp);
+    servAddr.sin_port = htons(port);
+    if(-1 == connect(sockFd, (struct sockaddr*)&servAddr, sizeof(struct sockaddr)))
+    {
+        printf("Connect failed!\n");
+        exit(-1);
+    }
+
+    printf("Connected success.\n");
+    len = recv(sockFd, buffer, MAX_BUFF_SIZE, 0);
+    buffer[len] = '\0';
+    printf("%s\n", buffer);
+
+    while(1)
+    {
+        printf("Input string send to server, end with \"end\":\n");
+        scanf("%s", buffer);
+        if(!strcmp(buffer, "end"))
+            break;
+        len = send(sockFd, buffer, strlen(buffer), 0);
+        len = recv(sockFd, buffer, MAX_BUFF_SIZE, 0);
+        buffer[len] = '\0';
+        printf("received: %s\n", buffer);
+
+    }
+
+    close(sockFd);
+
+    return 0;
+}
 
--- tcp/server.c
+++ tcp/server.c
@@ -1,3 +1,71 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+static const unsigned short port = 6666;
+#define MAX_BUFF_SIZE 4096
 
 
+int main(int ac, char** av)
+{
+    int serSockFd, cliSockFd;
+    int len;
+    struct sockaddr_in localAddr;
+    struct sockaddr_in remoteAddr;
+
+    int sinSize;
+    char buffer[MAX_BUFF_SIZE] = {0};
+
+    serSockFd = socket(AF_INET, SOCK_STREAM, 0);
+    if(serSockFd == -1)
+    {
+        printf("Create socket failed!\n");
+        exit(-1);
+    }
+
+    memset(&localAddr, 0, sizeof(struct sockaddr_in));
+    localAddr.sin_family = AF_INET;
+    localAddr.sin_addr.s_addr = INADDR_ANY;
+    localAddr.sin_port = htons(port);
+
+    if(-1 == bind(serSockFd, (struct sockaddr*)&localAddr, sizeof(struct sockaddr)))
+    {
+        printf("Bind error!\n");
+    }
+
+    listen(serSockFd, 5);
+    sinSize = sizeof(struct sockaddr_in);
+
+    // Waiting for clients' accepting.
+    cliSockFd = accept(serSockFd, (struct sockaddr*)&remoteAddr, &sinSize);
+    if(cliSockFd == -1)
+    {
+        printf("Accept error!\n");
+        exit(-1);
+    }
+
+    printf("Accepted client: %s\n", inet_ntoa(remoteAddr.sin_addr));
+    len = send(cliSockFd, "!!! WELCOME !!!\n", 21, 0);
+
+    while((len = recv(cliSockFd, buffer, MAX_BUFF_SIZE, 0)) > 0)
+    {
+        buffer[len] = '\0';
+        printf("%s\n", buffer);
+        if(-1 == send(cliSockFd, buffer, len, 0))
+        {
+            printf("Send error!\n");
+            break;;
+        }
+    }
+
+    close(cliSockFd);
+    close(serSockFd);
+
+
+    return 0;
+}
